813cbb7d9a030817ac7cef70faa79edefaa018b5
Tim Murphy <tim@26tp.com>
2023-12-26 16:00:27 +1100

    Add CommonAssemblyInfo

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Tim Murphy")]
+[assembly: AssemblyProduct("CroquetScores")]
+[assembly: AssemblyCopyright("Copyright © Tim Murphy 2016 - 2023")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
