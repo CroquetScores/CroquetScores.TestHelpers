29fcf95e4111d12416980b297734bfdc41dbac14
Tim Murphy <tim@26tp.com>
2023-12-27 20:30:20 +1100

    Make WebServer public

--- source/CroquetScores.TestHelpers/Services/WebServer.cs
+++ source/CroquetScores.TestHelpers/Services/WebServer.cs
@@ -4,11 +4,11 @@ using CroquetScores.TestHelpers.Support;
 
 namespace CroquetScores.TestHelpers.Services
 {
-    internal class WebServer
+    public class WebServer
     {
         private static readonly ConcurrentDictionary<int, HttpProcess> Servers = new ConcurrentDictionary<int, HttpProcess>();
 
-        internal static void StartIfNotRunning(string scheme, int port, TimeSpan maximumWaitTimeForProcessToRespond = default(TimeSpan))
+        public static void StartIfNotRunning(string scheme, int port, TimeSpan maximumWaitTimeForProcessToRespond = default(TimeSpan))
         {
             Servers.GetOrAdd(port, p => new WebServerProcess(scheme, port, maximumWaitTimeForProcessToRespond).StartIfNotRunning());
         }
