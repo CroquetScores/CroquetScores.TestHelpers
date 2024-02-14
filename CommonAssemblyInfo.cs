7dbf81f50fe0b5e23c575d142fd8afd91e4aa4cd
Tim Murphy <tim@26tp.com>
2024-02-14 20:13:52 +1100

    Remove ravedb and azurestorage (#7)
    * Remove RavenDB & Azure Storage and update NuGet packages
    
    * Update OpenMagic NuGet package
    
    * Update version number

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Tim Murphy")]
 [assembly: AssemblyProduct("CroquetScores")]
-[assembly: AssemblyCopyright("Copyright © Tim Murphy 2016 - 2023")]
+[assembly: AssemblyCopyright("Copyright © Tim Murphy 2016 - 2024")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("6.2.2.0")]
-[assembly: AssemblyFileVersion("6.2.2.0")]
+[assembly: AssemblyVersion("7.0.0.0")]
+[assembly: AssemblyFileVersion("7.0.0.0")]
--- source/CroquetScores.TestHelpers/Services/WebSite.cs
+++ source/CroquetScores.TestHelpers/Services/WebSite.cs
@@ -6,8 +6,6 @@ namespace CroquetScores.TestHelpers.Services
     {
         public static void StartIfNotRunning(string webserverScheme = "https", int webserverPort = 44301, TimeSpan maximumWaitTimeForProcessToRespond = default(TimeSpan))
         {
-            WindowsAzureStorageEmulator.StartIfNotRunning();
-            RavenServer.StartIfNotRunning(8080);
             WebServer.StartIfNotRunning(webserverScheme, webserverPort, maximumWaitTimeForProcessToRespond);
         }
     }
